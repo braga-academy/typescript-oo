b4dde4df2b8dbc100a82d6a18de0e66e0f6e530a
lucianobragaweb <lucianobragaweb@gmail.com>
2022-07-04 23:49:26 -0300

    understanding about heritage

---
+++ aula04-heranca/Util.ts
@@ -0,0 +1,5 @@
+export class Util {
+    public static randomizar(inicio: number, fim: number) {
+        return inicio + Math.random() * fim
+    } 
+}
--- aula04-heranca/main.ts
+++ aula04-heranca/main.ts
@@ -1,51 +1,12 @@
-import prompt from 'prompt-sync'
-import Personagem from './Personagem'
+import { Mage } from "./Mage";
+import { Personagem } from "./Personagem";
+import { Priest } from "./Priest";
+import { Warior } from "./Warrior";
 
-let person: Personagem
-person = new Personagem('Sansa Stark', 40, 50, 20, 10)
+let mage: Personagem = new Mage('Lady Melisandre')
+let warrior: Personagem = new Warior('Jon Snow')
+let priest: Personagem = new Priest('Samwell Tarly')
 
-let teclado = prompt()
-let option: number = 0
-
-while (option != 9 || person.isDead()) {
-    console.log(`+======== ${person.nome} ============+`)
-    console.log('| 1. Treinar ataque              |')
-    console.log('| 2. Treinar defesa              |')
-    console.log('| 3. Descansar                   |')
-    console.log('| 4. Entrar em batalha           |')
-    console.log('| 8. Imprimir atributos          |')
-    console.log('| 9. Sair                        |')
-    console.log('+================================+')
-    
-    option = +teclado('Escolha uma ação: ')
-
-    switch(option) {
-        case 1:
-            person.treinarAtaque()
-            console.log(person.status())         
-            break
-        case 2:
-            person.treinarDefesa()
-            console.log(person.status())
-            break
-        case 3:
-            let horas: number = +teclado('Digite o número de horas: ')
-            person.descansar(horas)
-            console.log(person.status())
-            break
-        case 4:
-            let rest: number = person.batalhar()
-            console.log(`Esta batalha custou: ${rest} de energia.`)            
-            console.log(person.status())            
-            break
-        case 8:
-            console.log(person.status())
-            break
-        default:
-            break
-    }
-}
-
-console.log(`+=========================+`)
-console.log('|=== Ops! Você morreu! ===|')
-console.log(`+=========================+`)
+console.log('mage:', mage)
+console.log('warrior:', warrior)
+console.log('priest:', priest)
